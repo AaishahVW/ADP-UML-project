14e9278a1c73f74e863e71f2e909a599379f7d10
AaishahVW <127095681+AaishahVW@users.noreply.github.com>
2024-02-25 15:47:36 +0200

    Pojo & Test Class - Isha
    Complete pojo and test class code

--- adp-uml/src/main/java/za/ac/cput/domain/Student.java
+++ adp-uml/src/main/java/za/ac/cput/domain/Student.java
@@ -4,4 +4,58 @@ public class Student {
 
     private Course regCourse = new Course();
     private Person stuDetails = new Person();
+    private int studNo;
+    private int studYear;
+
+    public Student(Course regCourse, Person stuDetails, int studNo, int studYear) {
+        this.regCourse = regCourse;
+        this.stuDetails = stuDetails;
+        this.studNo = studNo;
+        this.studYear = studYear;
+    }
+
+    public Student() {
+    }
+
+    public Course getRegCourse() {
+        return regCourse;
+    }
+
+    public Person getStuDetails() {
+        return stuDetails;
+    }
+
+    public int getStudNo() {
+        return studNo;
+    }
+
+    public int getStudYear() {
+        return studYear;
+    }
+
+    public void setRegCourse(Course regCourse) {
+        this.regCourse = regCourse;
+    }
+
+    public void setStuDetails(Person stuDetails) {
+        this.stuDetails = stuDetails;
+    }
+
+    public void setStudNo(int studNo) {
+        this.studNo = studNo;
+    }
+
+    public void setStudYear(int studYear) {
+        this.studYear = studYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "regCourse=" + regCourse +
+                ", stuDetails=" + stuDetails +
+                ", studNo=" + studNo +
+                ", studYear=" + studYear +
+                '}';
+    }
 }
